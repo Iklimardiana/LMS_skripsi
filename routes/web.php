49418ca4514f6a77a42afd12daabe563b7003276
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2024-01-23 08:12:34 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
@@ -21,6 +22,24 @@ use App\Http\Controllers\AdminController;
 Route::get('/', function () {
     return view('index');
 });
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', [AdminController::class, 'dashboard']);
+    Route::get('/admin/teacher', [AdminController::class, 'teachers']);
+    Route::get('/admin/teacher/create', [AdminController::class, 'createTeachers']);
+    Route::post('/admin/teacher', [AdminController::class, 'storeTeacher']);
+    Route::delete('/admin/teacher/{id}', [AdminController::class, 'destroyTeacher'])->name('teacher.destroy');
+
+    Route::get('/admin/student', [AdminController::class, 'student']);
+    Route::delete('/admin/student/{id}', [AdminController::class, 'destroyStudent'])->name('student.destroy');
+
+    Route::resource('/admin/subject', SubjectController::class);
+});
+
+Route::middleware('teacher')->group(function () {
+    Route::get('/teacher', [TeacherController::class, 'dashboard']);
+    Route::get('/teacher/subject', [TeacherController::class, 'subjects']);
+    Route::get('teacher/subject/{id}/student', [TeacherController::class, 'students']);
+});
 
 Route::get('/login', [LoginController::class, 'login']);
 Route::post('/login', [LoginController::class, 'authenticate']);
@@ -35,15 +54,3 @@ Route::get('/forgot', [ResetPasswordController::class, 'forgot_password']);
 Route::post('/forgot', [ResetPasswordController::class, 'store']);
 Route::get('/reset/{token}', [ResetPasswordController::class, 'verify'])->name('verify');
 Route::post('/reset', [ResetPasswordController::class, 'reset']);
-
-Route::get('/admin', [AdminController::class, 'dashboard']);
-Route::get('/admin/teacher', [AdminController::class, 'teachers']);
-Route::get('/admin/teacher/create', [AdminController::class, 'createTeachers']);
-Route::post('/admin/teacher', [AdminController::class, 'storeTeacher']);
-Route::delete('/admin/teacher/{id}', [AdminController::class, 'destroyTeacher'])->name('teacher.destroy');
-
-Route::get('/admin/student', [AdminController::class, 'student']);
-Route::delete('/admin/student/{id}', [AdminController::class, 'destroyStudent'])->name('student.destroy');
-
-Route::resource('/admin/subject', SubjectController::class);
-// Route::resource('/admin/subject', AdminController::class);
