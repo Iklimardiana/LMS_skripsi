9976acbadb20d2946a833b7943ae6599b1035ff4
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2024-02-26 19:17:52 +0700

    update middleware

--- routes/web.php
+++ routes/web.php
@@ -130,16 +130,18 @@ Route::middleware('student')->group(function () {
     Route::post('student/finish-exam', [ExamStudentController::class, 'finishExam'])->name('exam.finisih');
 });
 
-Route::get('/login', [LoginController::class, 'login']);
-Route::post('/login', [LoginController::class, 'authenticate']);
-Route::get('/logout', [LoginController::class, 'logout']);
-Route::get('/login/verify', [LoginController::class, 'verify'])->name('verification.verify');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'login']);
+    Route::post('/login', [LoginController::class, 'authenticate']);
+    Route::get('/logout', [LoginController::class, 'logout']);
+    Route::get('/login/verify', [LoginController::class, 'verify'])->name('verification.verify');
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/register/{key}', [RegisterController::class, 'verify'])->name('verify');
+    Route::get('/register', [RegisterController::class, 'create']);
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::get('/register/{key}', [RegisterController::class, 'verify'])->name('verify');
 
-Route::get('/forgot', [ResetPasswordController::class, 'forgot_password']);
-Route::post('/forgot', [ResetPasswordController::class, 'store']);
-Route::get('/reset/{token}', [ResetPasswordController::class, 'verify'])->name('verify');
-Route::post('/reset', [ResetPasswordController::class, 'reset']);
+    Route::get('/forgot', [ResetPasswordController::class, 'forgot_password']);
+    Route::post('/forgot', [ResetPasswordController::class, 'store']);
+    Route::get('/reset/{token}', [ResetPasswordController::class, 'verify'])->name('verify');
+    Route::post('/reset', [ResetPasswordController::class, 'reset']);
+});
