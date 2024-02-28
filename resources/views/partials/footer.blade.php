cde279888a74adea2dbc4289f45ff508b8fcc727
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2024-02-28 20:23:49 +0700

    add email

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
-<footer
-    class="fixed bottom-0 left-0 z-40 w-full bg-gray-50 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-4">
-    <span class="text-sm text-gray-500 sm:text-center ">© 2024 Iklima Mardiana. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
-    </ul>
+<footer class="fixed bottom-0 left-0 z-40 w-full bg-gray-50 border-t border-gray-200 shadow">
+    <div class="flex items-center justify-between p-4">
+        <span class="text-sm text-gray-500 sm:text-center ">© 2024 MyEdu. All Rights Reserved.</span>
+        <ul class="items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="mailto:iklimardiana911@gmail.com" class="hover:underline">Contact</a>
+            </li>
+        </ul>
+    </div>
 </footer>
