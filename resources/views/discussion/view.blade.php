a760ce46efc2c6fc85ecfcd5af960f52be600d96
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2024-03-19 11:40:43 +0700

    add overflow for dropdown

--- resources/views/discussion/view.blade.php
+++ resources/views/discussion/view.blade.php
@@ -47,7 +47,7 @@
 
                 <!-- Dropdown menu -->
                 <div id="dropdown"
-                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 overflow-y-auto max-h-64">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                         <li>
                             <a href="{{ route('discussion.view', ['idSubject' => $subject->id, 'material' => '']) }}"
